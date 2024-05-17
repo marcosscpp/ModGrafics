2b6529c95034a1a2c41b0f8a1b526d58bea06e75
marcosscpp <marcosscpp@gmail.com>
2024-05-17 12:17:44 -0300

    Inserting missing files

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ SECRET_KEY = "GrupoDomModtecnologia"
 
 SQLALCHEMY_DATABASE_URI = \
     'mysql://{user}:{password}@{server}:{port}/{database}'.format(
-        user= os.environ['user'],
-        password=  os.environ['password'],
-        server=  os.environ['server'],
-        port=  os.environ['port'],
-        database='automacao'
+        user = os.environ['user'],
+        password = os.environ['password'],
+        server = os.environ['server'],
+        port = os.environ['port'],
+        database = "automacao"
     )
 
 DIR_PATH = os.path.dirname(os.path.abspath(__file__)) + "/static"
